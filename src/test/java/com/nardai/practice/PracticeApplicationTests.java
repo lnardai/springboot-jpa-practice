42e33ebc697ffc2bb55f01c39db31004b7bad38d
lnardai <laci.nardai@gmail.com>
2017-06-15 00:12:22 +0200

    added 2 spring integrations test for controllers

--- src/test/java/com/nardai/practice/PracticeApplicationTests.java
+++ src/test/java/com/nardai/practice/PracticeApplicationTests.java
@@ -1,16 +1,37 @@
 package com.nardai.practice;
 
+import com.nardai.practice.controller.HelloController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@WebMvcTest(HelloController.class)
 public class PracticeApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	public void contextLoads() {
+	public void greetingTest() throws Exception{
+		mockMvc.perform(post("/greetings"))
+				.andExpect(status().is2xxSuccessful())
+				.andExpect(content().string("Greetings from Spring Boot!"));
+	}
+
+	@Test
+	public void goodbyeTest() throws Exception{
+		mockMvc.perform(post("/goodbye"))
+				.andExpect(status().is2xxSuccessful())
+				.andExpect(content().string("Goodbye!"));
 	}
 
 }
