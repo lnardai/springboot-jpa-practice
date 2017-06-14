8b02a98043a8320b06ad7c2dbd938ee71bb36140
lnardai <laci.nardai@gmail.com>
2017-06-14 23:49:26 +0200

    initial commit with working spring container

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # springboot-jpa-practice
+
+
+To Hot deploying application start one process with continuous compilation:
+```
+gradle -t classes
+```
+Other process should start a Spring container
+```
+gradle bootRun
+```
---
+++ src/main/java/com/nardai/practice/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.nardai.practice.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/greetings")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+    @RequestMapping("/goodbye")
+    public String goodbye() {
+        return "Goodbye!";
+    }
+
+}
